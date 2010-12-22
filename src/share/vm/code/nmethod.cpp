16e50fb2b72f8e3de918208730c02efd6667d6fa
Thomas Wuerthinger <wuerthinger@ssw.jku.at>
2010-12-22 19:47:39 +0100

    Important fix for exception unwind method entry offset (regression introduced during update to latest OpenJDK). Hard to find bug => should add a regression test for wrong offsets.

--- src/share/vm/code/nmethod.cpp
+++ src/share/vm/code/nmethod.cpp
@@ -794,11 +794,11 @@ nmethod::nmethod(
 
     // Exception handler and deopt handler are in the stub section
     if (UseC1X) {
-      // c1x produces no stub section
-      _exception_offset        = _stub_offset          + offsets->value(CodeOffsets::Exceptions);
-      _deoptimize_offset       = _stub_offset          + offsets->value(CodeOffsets::Deopt);
+      // c1x produces no (!) stub section
+      _exception_offset        = code_offset()          + offsets->value(CodeOffsets::Exceptions);
+      _deoptimize_offset       = code_offset()          + offsets->value(CodeOffsets::Deopt);
       if (has_method_handle_invokes()) {
-        _deoptimize_mh_offset  = _stub_offset          + offsets->value(CodeOffsets::DeoptMH);
+        _deoptimize_mh_offset  = code_offset()          + offsets->value(CodeOffsets::DeoptMH);
       } else {
         _deoptimize_mh_offset  = -1;
       }
