86b5cb165592a119f828efe1bd4693bb37a38141
Lukas Stadler <lukas.stadler@jku.at>
2011-06-09 15:25:27 +0200

    RiTypeProfile information and invocation counters for RiMethods

--- src/cpu/x86/vm/c1_globals_x86.hpp
+++ src/cpu/x86/vm/c1_globals_x86.hpp
@@ -48,7 +48,7 @@ define_pd_global(intx, FreqInlineSize,               325  );
 define_pd_global(intx, NewSizeThreadIncrease,        4*K  );
 define_pd_global(intx, InitialCodeCacheSize,         160*K);
 define_pd_global(intx, ReservedCodeCacheSize,        32*M );
-define_pd_global(bool, ProfileInterpreter,           false);
+define_pd_global(bool, ProfileInterpreter,           true );    // changed for GRAAL
 define_pd_global(intx, CodeCacheExpansionSize,       32*K );
 define_pd_global(uintx,CodeCacheMinBlockLength,      1);
 define_pd_global(uintx,PermSize,                     12*M );
@@ -57,7 +57,7 @@ define_pd_global(bool, NeverActAsServerClassMachine, true );
 define_pd_global(uint64_t,MaxRAM,                    1ULL*G);
 define_pd_global(bool, CICompileOSR,                 true );
 #endif // !TIERED
-define_pd_global(bool, UseTypeProfile,               false);
+define_pd_global(bool, UseTypeProfile,               true );    // changed for GRAAL
 define_pd_global(bool, RoundFPResults,               true );
 
 define_pd_global(bool, LIRFillDelaySlots,            false);
