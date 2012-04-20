d6bcfe7434f7d211504ba6126e6ecbd9bd595782
Lukas Stadler <lukas.stadler@jku.at>
2012-04-20 15:02:54 +0200

    enable graph caching and priority compilation queue

--- src/cpu/x86/vm/c1_globals_x86.hpp
+++ src/cpu/x86/vm/c1_globals_x86.hpp
@@ -52,7 +52,7 @@ define_pd_global(bool, CICompileOSR,                 true );
 #ifdef GRAAL
 define_pd_global(bool, ProfileTraps,                 true );
 define_pd_global(bool, UseOnStackReplacement,        false);
-define_pd_global(intx, CompileThreshold,             4500 );
+define_pd_global(intx, CompileThreshold,             2500 );
 define_pd_global(intx, InitialCodeCacheSize,         4*M  );
 define_pd_global(intx, ReservedCodeCacheSize,        48*M );
 define_pd_global(bool, ProfileInterpreter,           true );
