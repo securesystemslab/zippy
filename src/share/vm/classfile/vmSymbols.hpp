d6b774c0b7ae92227eb709c0cc086fb0afd1127e
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2012-06-07 20:54:54 +0200

    C++ changes for Java renamings.

--- src/share/vm/classfile/vmSymbols.hpp
+++ src/share/vm/classfile/vmSymbols.hpp
@@ -282,8 +282,8 @@
   template(com_oracle_graal_hotspot_HotSpotKlassOop,              "com/oracle/graal/hotspot/ri/HotSpotKlassOop")                  \
   template(com_oracle_graal_hotspot_HotSpotExceptionHandler,      "com/oracle/graal/hotspot/ri/HotSpotExceptionHandler")          \
   template(com_oracle_graal_hotspot_HotSpotProxy,                 "com/oracle/graal/hotspot/HotSpotProxy")                        \
-  template(com_oracle_graal_hotspot_Compiler,                     "com/oracle/graal/hotspot/Compiler")                            \
-  template(com_oracle_graal_hotspot_CompilerImpl,                 "com/oracle/graal/hotspot/CompilerImpl")                        \
+  template(com_oracle_graal_hotspot_Compiler,                     "com/oracle/graal/hotspot/HotSpotCompiler")                            \
+  template(com_oracle_graal_hotspot_CompilerImpl,                 "com/oracle/graal/hotspot/HotSpotCompilerImpl")                        \
   template(com_oracle_max_cri_ri_RiMethod,                            "com/oracle/graal/api/meta/RiMethod")                                   \
   template(com_oracle_max_cri_ri_RiResolvedField,                     "com/oracle/graal/api/meta/RiResolvedField")                            \
   template(com_oracle_max_cri_ri_RiType,                              "com/oracle/graal/api/meta/RiType")                                     \
@@ -349,7 +349,7 @@
   template(getVMToCompiler_signature,                 "()Lcom/oracle/graal/hotspot/bridge/VMToCompiler;")               \
   template(getInstance_name,                          "getInstance")                                                    \
   template(initialize_name,                           "initialize")                                                     \
-  template(getInstance_signature,                     "()Lcom/oracle/graal/hotspot/Compiler;")                          \
+  template(getInstance_signature,                     "()Lcom/oracle/graal/hotspot/HotSpotCompiler;")                          \
   template(forObject_name,                            "forObject")                                                      \
   template(callbackInternal_name,                     "callbackInternal")                                               \
   template(callback_signature,                        "(Ljava/lang/Object;)Ljava/lang/Object;")                         \
