1c57bb4748e456e7505c9c9957214ac492c43d66
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2012-06-08 17:55:28 +0200

    Create HotSpotGraalRuntime class on HotSpot when Graal.getRuntime() is accessed.

--- src/share/vm/graal/graalRuntime.cpp
+++ src/share/vm/graal/graalRuntime.cpp
@@ -28,5 +28,9 @@ JVM_ENTRY(jobject, JVM_InitializeGraalRuntime(JNIEnv *env, jclass graalclass))
   ThreadToNativeFromVM ttnfv(thread);
   jclass klass = env->FindClass("com/oracle/graal/hotspot/HotSpotGraalRuntime");
   guarantee(klass != NULL, "Could not find class com.oracle.graal.hotspot.HotSpotGraalRuntime");
-  return env->AllocObject(klass);
+  jmethodID constructor = env->GetMethodID(klass, "<init>", "()V");
+  guarantee(constructor != NULL, "Constructor of HotSpotGraalRuntime must not have any parameters");
+  jobject newObj = env->AllocObject(klass);
+  env->CallVoidMethod(newObj, constructor);
+  return newObj;
 JVM_END
