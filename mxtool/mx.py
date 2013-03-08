08fbc56a820fb38362cfbed7bf0e7ca1c278b8d2
Lukas Stadler <lukas.stadler@jku.at>
2013-03-08 14:38:42 +0100

    pass JAVA_HOME to eclipse builder

--- mxtool/mx.py
+++ mxtool/mx.py
@@ -2258,6 +2258,10 @@ def _isAnnotationProcessorDependency(p):
 def _genEclipseBuilder(dotProjectDoc, p, name, mxCommand, refresh=True, async=False):
     launchOut = XMLDoc();
     launchOut.open('launchConfiguration', {'type' : 'org.eclipse.ui.externaltools.ProgramBuilderLaunchConfigurationType'})
+    launchOut.open('mapAttribute', {'key' : 'org.eclipse.debug.core.environmentVariables'})
+    launchOut.element('mapEntry', {'key' : 'JAVA_HOME',	'value' : java().jdk})
+    launchOut.close('mapAttribute')
+    
     if refresh:
         launchOut.element('stringAttribute',  {'key' : 'org.eclipse.debug.core.ATTR_REFRESH_SCOPE',            'value': '${project}'})
     launchOut.element('booleanAttribute', {'key' : 'org.eclipse.debug.core.capture_output',                'value': 'false'})
