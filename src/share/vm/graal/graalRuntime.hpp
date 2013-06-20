8bb9fd08730d9031029b46cf2188022f91ec14a6
Christos Kotselidis <christos.kotselidis@oracle.com>
2013-06-20 16:10:14 +0200

    Add G1 Barriers' foreign calls

--- src/share/vm/graal/graalRuntime.hpp
+++ src/share/vm/graal/graalRuntime.hpp
@@ -52,6 +52,8 @@ class GraalRuntime: public AllStatic {
     LOG_OBJECT_ADDRESS = 0x04
   };
   static void log_object(JavaThread* thread, oop msg, jint flags);
+  static void write_barrier_pre(JavaThread* thread, oopDesc* obj);
+  static void write_barrier_post(JavaThread* thread, void* card);
 };
 
 #endif // SHARE_VM_GRAAL_GRAAL_RUNTIME_HPP
