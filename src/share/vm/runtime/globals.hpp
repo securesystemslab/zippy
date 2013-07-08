7dd05bdcd3d57120017b73ff91098e9ff2460270
Christian Haeubl <haeubl@ssw.jku.at>
2013-07-08 09:57:14 +0200

    Reverted attempt to increase accuracy of profiling information (57b9a8c7cea8).

--- src/share/vm/runtime/globals.hpp
+++ src/share/vm/runtime/globals.hpp
@@ -3483,7 +3483,7 @@ class CommandLineFlags {
           "NON_TIERED number of method invocations/branches (expressed as %"\
           "of CompileThreshold) before (re-)compiling OSR code")            \
                                                                             \
-  product(intx, InterpreterProfilePercentage, NOT_GRAAL(33) GRAAL_ONLY(3),  \
+  product(intx, InterpreterProfilePercentage, 33,                           \
           "NON_TIERED number of method invocations/branches (expressed as %"\
           "of CompileThreshold) before profiling in the interpreter")       \
                                                                             \
