d4c4a5aa06001dc6730c8cfae922c41c4e292733
ankitv <ankitv@uci.edu>
2013-10-10 14:47:39 -0700

    added new unit tests and .py files. please check BinaryArithTest unit.

--- graal/edu.uci.python.test/src/tests/binary_arith_test.py
+++ graal/edu.uci.python.test/src/tests/binary_arith_test.py
@@ -1,33 +1,33 @@
 # basic binary ops
-print "----------- basic + - * / //"
+print ("----------- basic + - * / //")
 a = 1 + 2
 b = 4 - 2
 c = 6 * 2
 d = 7 / 2
 e = 7 // 2
-print a, b, c, d, e
+print (a, b, c, d, e)
 
 # slightly more complex ones
-print "----------- double/BigInteger + - * / //"
+print ("----------- double/BigInteger + - * / //")
 a = 345606 + 364 * 2
 b = 1101010101 / 356 - 2002
 c = 42 - 99999 * 543858438584385
 d = 1 / 356 * 2.0
 e = 3 // 5.0
-print a, b, c, d, e
+print (a, b, c, d, e)
 
 # more complicated ones
-print "----------- more double/BigInteger / // **"
+print ("----------- more double/BigInteger / // **")
 a = 46372573068954628579 / 432
 b = 46372573068954628579 / 43.2
 c = (3.56 - 5278948673290672067) // 6427069
 d = 2 ** 4
 e = 2.5 ** 3.0
-print a, b, c, d, e
+print (a, b, c, d, e)
 
 # modulo test
-print "----------- %"
+print ("----------- %")
 a = 14 % 5
 b = 54528840284285205820 % 52
 c = 43253252 % 0.7
-print a, b, c
+print (a, b, c)
