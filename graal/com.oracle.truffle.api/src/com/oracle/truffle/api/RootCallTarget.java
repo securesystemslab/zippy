100b1966d817383c0b2b67b79325026c4cd1a9e5
Christian Wimmer <christian.wimmer@oracle.com>
2014-01-24 18:13:38 -0800

    Truffle API cleanup: Reduce the visibility of classes and constructors that are not intended to be instantiated by guest language implementations; provide abstract class RootCallTarget as a CallTarget to a RootNode

--- graal/com.oracle.truffle.api/src/com/oracle/truffle/api/RootCallTarget.java
+++ graal/com.oracle.truffle.api/src/com/oracle/truffle/api/RootCallTarget.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -20,25 +22,29 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package com.oracle.graal.truffle;
+package com.oracle.truffle.api;
 
-import com.oracle.graal.debug.*;
-import com.oracle.truffle.api.*;
 import com.oracle.truffle.api.nodes.*;
 
-public class TruffleTreeDumpHandler implements DebugDumpHandler {
+/**
+ * Represents the target of a call to a {@link RootNode}, i.e., to another tree of nodes. Instances
+ * of this class can be created using {@link TruffleRuntime#createCallTarget(RootNode)}.
+ */
+public abstract class RootCallTarget extends CallTarget {
 
-    @Override
-    public void dump(Object object, final String message) {
-        if (object instanceof RootCallTarget) {
-            RootCallTarget callTarget = (RootCallTarget) object;
-            if (callTarget.getRootNode() != null) {
-                new GraphPrintVisitor().beginGroup(callTarget.toString()).beginGraph(message).visit(callTarget.getRootNode()).printToNetwork();
-            }
-        }
+    private final RootNode rootNode;
+
+    public RootCallTarget(RootNode function) {
+        this.rootNode = function;
+        this.rootNode.setCallTarget(this);
     }
 
-    public void close() {
-        // nothing to do
+    @Override
+    public String toString() {
+        return rootNode.toString();
+    }
+
+    public RootNode getRootNode() {
+        return rootNode;
     }
 }
