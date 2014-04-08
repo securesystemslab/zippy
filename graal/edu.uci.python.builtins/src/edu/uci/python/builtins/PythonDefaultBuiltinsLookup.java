8ca7b9053719a13fc955a79749dbf5696b02813c
Wei Zhang <ndrzmansn@gmail.com>
2014-04-07 21:47:20 -0700

    Built-in classes and modules initial object layout checks

--- graal/edu.uci.python.builtins/src/edu/uci/python/builtins/PythonDefaultBuiltinsLookup.java
+++ graal/edu.uci.python.builtins/src/edu/uci/python/builtins/PythonDefaultBuiltinsLookup.java
@@ -3,14 +3,14 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- * 
+ * modification, are permitted provided that the following conditions are met:
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
- * 
+ *    and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -32,6 +32,7 @@ import edu.uci.python.runtime.*;
 import edu.uci.python.runtime.builtin.*;
 import edu.uci.python.runtime.datatype.*;
 import edu.uci.python.runtime.function.*;
+import edu.uci.python.runtime.object.*;
 import edu.uci.python.runtime.sequence.*;
 import edu.uci.python.runtime.standardtype.*;
 
@@ -75,6 +76,8 @@ public final class PythonDefaultBuiltinsLookup implements PythonBuiltinsLookup {
 
     private static PythonModule createModule(String name, PythonContext context, PythonBuiltins... builtins) {
         PythonModule module = new PythonModule(context, name, null);
+        assert module.usePrivateLayout() && module.getObjectLayout() == ObjectLayout.EMPTY;
+
         for (PythonBuiltins builtin : builtins) {
             addBuiltinsToModule(module, builtin, context);
         }
--- graal/edu.uci.python.runtime/src/edu/uci/python/runtime/PythonContext.java
+++ graal/edu.uci.python.runtime/src/edu/uci/python/runtime/PythonContext.java
@@ -3,14 +3,14 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- * 
+ * modification, are permitted provided that the following conditions are met:
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
- * 
+ *    and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -32,6 +32,7 @@ import com.oracle.truffle.api.nodes.*;
 import com.oracle.truffle.api.source.*;
 
 import edu.uci.python.runtime.builtin.*;
+import edu.uci.python.runtime.object.*;
 import edu.uci.python.runtime.sequence.*;
 import edu.uci.python.runtime.standardtype.*;
 
@@ -65,6 +66,11 @@ public class PythonContext {
         this.objectClass = new PythonObjectClass(this);
         this.typeClass.unsafeSetSuperClass(objectClass);
         this.moduleClass = new PythonBuiltinClass(this, objectClass, "module");
+
+        assert typeClass.usePrivateLayout() && typeClass.getObjectLayout() == ObjectLayout.EMPTY;
+        assert objectClass.usePrivateLayout() && objectClass.getObjectLayout() == ObjectLayout.EMPTY;
+        assert moduleClass.usePrivateLayout() && moduleClass.getObjectLayout() == ObjectLayout.EMPTY;
+
         this.sourceManager = new SourceManager();
         this.parser = parser;
         this.importManager = new ImportManager(this);
