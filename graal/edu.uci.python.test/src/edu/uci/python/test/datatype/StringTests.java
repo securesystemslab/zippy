d3f4caf00f410cf64bc1290bb37e0add97bfb41e
Wei Zhang <ndrzmansn@gmail.com>
2014-04-08 13:19:11 -0700

    Clean up in unittest

--- graal/edu.uci.python.test/src/edu/uci/python/test/datatype/StringTests.java
+++ graal/edu.uci.python.test/src/edu/uci/python/test/datatype/StringTests.java
@@ -3,14 +3,14 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- * 
+ * modification, are permitted provided that the following conditions are met:
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
- * 
+ *    and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -60,10 +60,18 @@ public class StringTests {
     }
 
     @Test
-    public void join() {
+    public void join0() {
         String source = "s = set(str(i) for i in range(3))\n" + //
                         "print(''.join(s))\n";
         assertPrints("210\n", source);
+    }
+
+    @Test
+    public void join1() {
+        String source = "val = \"-\".join(\"12345\")\n" + //
+                        "print(val)\n";
+
+        assertPrints("1-2-3-4-5\n", source);
     }
 
     @Test
--- graal/edu.uci.python.test/src/tests/builtin_call_test.py
+++
@@ -1,5 +0,0 @@
-# zwei 10/02/2013
-# test builtin call nodes
-
-int42 = int(42)
-print(int42)
