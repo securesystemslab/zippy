5db7377323bf6311465cc92ba06623730cfc7798
Wei Zhang <ndrzmansn@gmail.com>
2014-04-08 13:23:23 -0700

    Class relocation

--- graal/edu.uci.python.test/src/edu/uci/python/test/grammar/CallTests.java
+++ graal/edu.uci.python.test/src/edu/uci/python/test/grammar/CallTests.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package edu.uci.python.test;
+package edu.uci.python.test.grammar;
 
 import static edu.uci.python.test.PythonTests.*;
 
