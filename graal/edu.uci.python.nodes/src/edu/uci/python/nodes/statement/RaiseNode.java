af9d3ec1576fafc15a68cb9ae4744da8ab70aa7f
Wei Zhang <ndrzmansn@gmail.com>
2014-04-17 23:43:36 -0700

    Use CompilerDirectives to exclude RaiseNode.execute from compiled code. This change fixed the compiler complaint caused by a previous commit

--- graal/edu.uci.python.nodes/src/edu/uci/python/nodes/statement/RaiseNode.java
+++ graal/edu.uci.python.nodes/src/edu/uci/python/nodes/statement/RaiseNode.java
@@ -3,14 +3,14 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- * 
+ * modification, are permitted provided that the following conditions are met:
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
- * 
+ *    and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -26,6 +26,7 @@ package edu.uci.python.nodes.statement;
 
 import org.python.core.*;
 
+import com.oracle.truffle.api.*;
 import com.oracle.truffle.api.CompilerDirectives.SlowPath;
 import com.oracle.truffle.api.frame.*;
 
@@ -47,6 +48,8 @@ public class RaiseNode extends StatementNode {
 
     @Override
     public Object execute(VirtualFrame frame) {
+        CompilerDirectives.transferToInterpreterAndInvalidate();
+
         Object t = (type == null) ? null : type.execute(frame);
         Object i = (inst == null) ? null : inst.execute(frame);
         /**
