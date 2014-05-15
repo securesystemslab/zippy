6b6c9fddcb4520b243ddfdddc243844e7ed8bf24
Doug Simon <doug.simon@oracle.com>
2014-05-15 22:42:08 +0200

    use JDK with highest compliance level for generated Eclipse attach launcher

--- mxtool/mx.py
+++ mxtool/mx.py
@@ -2994,7 +2994,7 @@ def _source_locator_memento(deps):
         elif dep.isProject():
             memento = XMLDoc().element('javaProject', {'name' : dep.name}).xml(standalone='no')
             slm.element('container', {'memento' : memento, 'typeId':'org.eclipse.jdt.launching.sourceContainer.javaProject'})
-            if javaCompliance is None or dep.javaCompliance < javaCompliance:
+            if javaCompliance is None or dep.javaCompliance > javaCompliance:
                 javaCompliance = dep.javaCompliance
 
     if javaCompliance:
