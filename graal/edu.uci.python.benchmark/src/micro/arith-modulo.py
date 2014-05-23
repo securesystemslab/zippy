6d8376a3f001c96e5afa6c0cf448d0d71efabdb4
Wei Zhang <ndrzmansn@gmail.com>
2014-05-23 14:42:56 -0700

    Add micro bench arith-modulo

---
+++ graal/edu.uci.python.benchmark/src/micro/arith-modulo.py
@@ -0,0 +1,40 @@
+# zwei 10/09/13
+# arithmetic ops (partially extracted from spectralnorm)
+import time
+
+def docompute(num):
+	for i in range(num):
+		sum = 0
+		j = 0
+
+		# if i == 0:
+		# 	i += 1
+
+		while j < i:
+			if i % 3 == 0:
+				temp = 1
+			else:
+				temp = i % 3
+			
+			j += temp
+		sum = sum + j
+
+	return sum
+
+
+def measure(num):
+	print("Start timing...")
+	start = time.time()
+
+	for run in range(num):
+		sum = docompute(5000) #5000
+
+	print("sum", sum)
+
+	duration = "%.3f\n" % (time.time() - start)
+	print("arith-modulo: " + duration)
+
+for run in range(50):
+	docompute(1000) #1000
+
+measure(50)
