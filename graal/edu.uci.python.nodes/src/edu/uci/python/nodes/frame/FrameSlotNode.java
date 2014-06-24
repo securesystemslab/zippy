d28d1327f974b0bcfba9d7267975df850513453a
Wei Zhang <ndrzmansn@gmail.com>
2014-06-24 16:58:30 -0700

    Remove FrameSlotNode.booleanToInt() again

--- graal/edu.uci.python.nodes/src/edu/uci/python/nodes/frame/FrameSlotNode.java
+++ graal/edu.uci.python.nodes/src/edu/uci/python/nodes/frame/FrameSlotNode.java
@@ -89,7 +89,7 @@ public abstract class FrameSlotNode extends PNode {
     }
 
     protected final boolean isIntegerKind() {
-        return isKind(FrameSlotKind.Int) || booleanToInt();
+        return isKind(FrameSlotKind.Int);
     }
 
     protected final boolean isDoubleKind() {
@@ -132,15 +132,6 @@ public abstract class FrameSlotNode extends PNode {
         if (frameSlot.getKind() == FrameSlotKind.None) {
             CompilerDirectives.transferToInterpreter();
             frameSlot.setKind(kind);
-            return true;
-        }
-        return false;
-    }
-
-    private boolean booleanToInt() {
-        if (frameSlot.getKind() == FrameSlotKind.Boolean) {
-            CompilerDirectives.transferToInterpreter();
-            frameSlot.setKind(FrameSlotKind.Int);
             return true;
         }
         return false;
